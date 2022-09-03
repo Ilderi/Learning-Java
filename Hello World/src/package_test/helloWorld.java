5cf6a82e0a83cbf737894ae6173a270d5026235b
Ilderi <krystian.kiecana@wp.pl>
2022-09-03 19:52:52 +0200

    Adding files

---
+++ Hello World/src/package_test/helloWorld.java
@@ -0,0 +1,11 @@
+package package_test;
+
+public class helloWorld {
+	
+	public static void main(String[] args) {
+		System.out.print("\"test\"\n");
+		System.out.println("new line");
+		
+		
+	}
+}
