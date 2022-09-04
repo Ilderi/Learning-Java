b67d7283da493c66be0cd4f26c50a017e8fc8694
Ilderi <krystian.kiecana@wp.pl>
2022-09-04 11:06:54 +0200

    Learning about expressions

---
+++ Expressions/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// expression = operands & operators
+		// operators = + - * / %
+		// operands = values, variables, numbers, quantity
+		
+		int friends = 10;
+		
+		System.out.println(friends);
+		friends++;
+		System.out.println(friends);
+		++friends;
+		System.out.println(friends);
+		friends+=2;
+		System.out.println(friends);
+		System.out.println((double)friends/5);
+
+	}
+
+}
